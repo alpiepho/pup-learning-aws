af156af8bc04157658fb313e763f6b20b6933624
al <alpiepho@yahoo.com>
2020-06-05 07:13:12 -0600

    created html and mdx pages from gathered json

--- main.js
+++ main.js
@@ -3,176 +3,169 @@ fs = require('fs');
 base = require('./base');
 site = require('./site');
 
-// const HTML_FILE = "./public/index.html";
-// const MD_FILE = "./artifacts/learning-summary.mdx";
+const HTML_FILE = "./public/index.html";
+const MD_FILE = "./artifacts/learning-aws-summary.mdx";
 const SCREENSHOT_DIR = "./screenshots";
 
-// const html1 = `
-// <!DOCTYPE html>
-// <html lang="en">
-//   <head>
-//     <meta charset="utf-8" />
-//     <meta name="viewport" content="width=device-width", initial-scale=1.0"/>
-//     <meta name="Description" content="LinkedIn Learning Courses Completed">
-//     <meta name="theme-color" content="#d36060"/>
-//     <title>
-//     LinkedIn Learning Courses Completed
-//     </title>
-//     <link rel="stylesheet" href="./style.css" />
-//     <link rel="manifest" href="./manifest.json" />
-//     <link rel="icon"
-//       type="image/png" 
-//       href="./favicon.ico" />
-//   </head>
-//   <body class="body">
-//     <main>
-//     <article class="page">
-//       <h1  id=\"top\">LinkedIn Learning Courses Completed</h1>
+const html1 = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width", initial-scale=1.0"/>
+    <meta name="Description" content="LinkedIn Learning Courses Completed">
+    <meta name="theme-color" content="#d36060"/>
+    <title>
+    AWS Training Courses Completed
+    </title>
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="manifest" href="./manifest.json" />
+    <link rel="icon"
+      type="image/png" 
+      href="./favicon.ico" />
+  </head>
+  <body class="body">
+    <main>
+    <article class="page">
+      <h1  id=\"top\">AWS Training Courses Completed</h1>
 
-//       <div class="introduction">
-//       <p>
-//       This a summary of all the Linked-In courses I have completed. 
-//       This is just the direct LinkedIn Learning courses.  There are a number of "Lynda.com"
-//       courses that were taken before subscribing to the LinkedIn premium plan.
-//       </p>
-//       <p>
-//       This list is generated from a tool called "pup-learning" that can be found
-//       <a
-//         href="https://github.com/alpiepho/pup-learning"
-//         target="_blank"
-//         rel="noreferrer"
-//       >here</a>.  This tool needs to be run manually to parse the LinkedIn Learning
-//       site to gather the list of courses I have taken.
-//       </p>
-//       <p>
-//         If you look over the list of courses, there is variety.  I fully admit that
-//         my attention for some courses was less that other.  My form of bing watching :) 
-//       </p>
-//       </div>
-// `;
+      <div class="introduction">
+      <p>
+      This a summary of all the AWS Training courses I have completed.  These are the
+      free online courses.
+      </p>
+      <p>
+      This list is generated from a tool called "pup-learning-aws" that can be found
+      <a
+        href="https://github.com/alpiepho/pup-learning-aws"
+        target="_blank"
+        rel="noreferrer"
+      >here</a>.  This tool needs to be run manually to parse the AWS Training
+      site to gather the list of courses I have taken.
+      </p>
+      </div>
+`;
 
-// const html2 = `
-//     <div id=\"bottom\"></div>
-//     </article>
-//   </body>
-// </html>
-// `;
+const html2 = `
+    <div id=\"bottom\"></div>
+    </article>
+  </body>
+</html>
+`;
 
-// const md1 = `---
-// title: LinkedIn Completed Courses
-// date: "2020-04-24"
-// description: "Summary of my LinkedIn Learning Completed Courses"
-// ---
+const md1 = `---
+title: LinkedIn Completed Courses
+date: "2020-06-04"
+description: "Summary of my AWS Training Completed Courses"
+---
 
-// (Warning: many images) This a summary of all the Linked-In courses I have completed. 
-// This is just the direct LinkedIn Learning courses.  There are a number of "Lynda.com"
-// courses that were taken before subscribing to the LinkedIn premium plan.
+This a summary of all the AWS Training courses I have completed.  These are the
+free online courses.
 
-// A full summary with more details can be found [here](https://alpiepho.github.io/pup-learning/).
+A full summary with more details can be found [here](https://alpiepho.github.io/pup-learning-aws/).
 
-// #### top
+#### top
 
-// `;
+`;
 
-// const md2 = `
+const md2 = `
 
-// #### bottom
-// `;
+#### bottom
+`;
 
-// function build_hours_minutes(data) {
-//   // Derive timestamps and duration, sort
-//   let totalSec = 0;
-//   data['completed-courses'].forEach(entry => {
-//     // assume "An Bm" or "Bm"
-//     let parts = entry['duration'].split(' ');
-//     for (i=0; i<parts.length; i++) {
-//       if (parts[i].includes('h')) {
-//         val = parseInt(parts[i].replace('h', ''));
-//         totalSec += val*60*60; 
-//       }
-//       if (parts[i].includes('m')) {
-//         val = parseInt(parts[i].replace('m', ''));
-//         totalSec += val*60; 
-//       }
-//       if (parts[i].includes('s')) {
-//         val = parseInt(parts[i].replace('s', ''));
-//         totalSec += val; 
-//       }
-//     }
-//     entry['released-ts'] = Date.parse(entry['released-date']);
-//     entry['completed-ts'] = Date.parse(entry['completed-date']);
-//   });
+function build_hours_minutes(data) {
+  // Derive timestamps and duration, sort
+  // examples
+  // "duration": "5 1/2 HOURS",
+  // "duration": "2 HOURS",
+  // "duration": "100 MINUTES",
 
-//   let totalMin = Math.floor(totalSec / 60);
-//   totalH = Math.floor(totalMin / 60); 
-//   totalM = totalMin - (totalH*60);
-//   return [totalH, totalM];
-// }
+  let totalSec = 0;
+  data['completed-courses'].forEach(entry => {
+    // assume "An Bm" or "Bm"
+    let parts = entry['duration'].split(' ');
+    if (parts.length == 2) {
+      val = parseInt(parts[0]);
+      if (parts[1] == "MINUTES")
+        totalSec += val*60; 
+      if (parts[1] == "HOURS")
+        totalSec += val*60*60; 
+    }
+    if (parts.length == 3) {
+      val = parseInt(parts[0]);
+      totalSec += val*60*60; 
+      totalSec += 30*60; 
+    }
+    entry['completed-ts'] = Date.parse(entry['registration-date']); // assume registration and completion are close
+  });
 
-// function build_html(data, totalH, totalM) {
-//   // generate artifacts from data - html
-//   let htmlStr = html1;
-//   htmlStr += "      <br/><p>Totals - Course: " + data['completed-courses'].length + ", Time: " + totalH + "h " + totalM + "m</p><br/>\n\n";
-//   htmlStr += "      <ul>\n";
-//   data['completed-courses'].forEach(entry => {
-//     htmlStr += "            <li>\n";
-//     htmlStr += "              <ul>\n";
-//     htmlStr += "                <li>\n";
+  let totalMin = Math.floor(totalSec / 60);
+  totalH = Math.floor(totalMin / 60); 
+  totalM = totalMin - (totalH*60);
+  return [totalH, totalM];
+}
 
-//     htmlStr += "                  <p><img src=\"" + entry['img'] + "\"</img></p>\n";
+function build_html(data, totalH, totalM) {
+  // generate artifacts from data - html
+  let htmlStr = html1;
+  htmlStr += "      <br/><p>Totals - Course: " + data['completed-courses'].length + ", Time: " + totalH + "h " + totalM + "m</p><br/>\n\n";
+  htmlStr += "      <ul>\n";
+  data['completed-courses'].forEach(entry => {
+    htmlStr += "            <li>\n";
+    temp = "";
+    if (entry['type'] ==  "E-LEARNING") temp = "hr_e_learning";
+    if (entry['type'] ==  "LEARNING PATH") temp = "hr_learning_path";
+    if (entry['type'] ==  "CURRICULUM") temp = "hr_curriculum";
+    if (entry['type'] ==  "VIDEO") temp = "hr_video";
+    if (entry['type'] ==  "COURSE") temp = "hr_course";
+    htmlStr += "              <hr class=\"" + temp + "\">\n";
+    htmlStr += "              <ul>\n";
+    htmlStr += "                <li>\n";
+    htmlStr += "                  <a target=\"_blank\" href=\"" + entry['link'] + "\">\n";
+    htmlStr += "                    " + entry['title'] + "\n";
+    htmlStr += "                  </a>  ";
+    htmlStr += "                </li>\n";
+    htmlStr += "                <li class=\"type\">" + entry['type'].toLowerCase() + "</li>\n";
+    htmlStr += "                <li class=\"level\">" + entry['level'].toLowerCase() + "</li>\n";
+    htmlStr += "                <li class=\"duration\">" + entry['duration'].toLowerCase() + "</li>\n";
+    htmlStr += "                <li class=\"completed\"><i>Completed: " + entry['registration-date'] + "</i></li>\n";
+    htmlStr += "                <li class=\"description\">" + entry['description'] + "</li>\n";
+    htmlStr += "                <li class=\"topbottom\"><a href=\"#top\">top</a> / <a href=\"#bottom\">bottom</a></li>\n";
+    htmlStr += "              </ul>\n";
+    htmlStr += "            </li>\n";
+  });
+  htmlStr += "      </ul>";
+  htmlStr += html2;
+  fs.writeFileSync(HTML_FILE, htmlStr);
+}
 
+function build_md(data, totalH, totalM) {
+  // generate markdown (.mdx) for blog
+  let mdStr = md1;
+  mdStr += "Total Completed Courses: " + data['completed-courses'].length + ", Time: " + totalH + "h " + totalM + "m\n";
+  mdStr += "<br/>\n";
+  mdStr += "<br/>\n";
+  mdStr += "<br/>\n";
+  mdStr += "\n";
+  data['completed-courses'].forEach(entry => {
+    mdStr += "\n";
+    mdStr += "\n";
+    mdStr += "[" + entry['title'] + "](" + entry['link'] + ")\n";
+    mdStr += "- Type: " + entry['type'].toLowerCase() + "\n";
+    mdStr += "- Level: " + entry['level'].toLowerCase() + "\n";
+    mdStr += "- Duration: " + entry['duration'].toLowerCase() + "\n";
+    mdStr += "- Completed: " + entry['registration-date'] + "\n";
+    mdStr += "- Description: " + entry['description'] + "\n";
+    mdStr += "- [top](#top) / [bottom](#bottom)\n";
 
-//     htmlStr += "                  <a target=\"_blank\" href=\"" + entry['link'] + "\">\n";
-//     htmlStr += "                    " + entry['title'] + "\n";
-//     htmlStr += "                  </a>  ";
-//     htmlStr += "                <span>(" + entry['released-date'].replace('Updated ','') + " ... " + entry['duration'] + ")</span>\n";
-//     htmlStr += "                </li>\n";
-//     if (entry['linkedin']) {
-//       htmlStr += "                <li><a target=\"_blank\" href=\"" + entry['linkedin'] + "\">" + entry['author'] + "</a></li>\n";
-//     } else {
-//       htmlStr += "                <li>Author: " + entry['author'] + "</li>\n";
-//     }
-//     htmlStr += "                <li class=\"completed\"><i>Completed: " + entry['completed-date'] + "</i></li>\n";
-//     htmlStr += "                <li class=\"details\">" + entry['details'] + "</li>\n";
-//     htmlStr += "                <li class=\"topbottom\"><a href=\"#top\">top</a> / <a href=\"#bottom\">bottom</a></li>\n";
-//     htmlStr += "              </ul>\n";
-//     htmlStr += "            </li>\n";
-//   });
-//   htmlStr += "      </ul>";
-//   htmlStr += html2;
-//   fs.writeFileSync(HTML_FILE, htmlStr);
-// }
-
-// function build_md(data, totalH, totalM) {
-//   // generate markdown (.mdx) for blog
-//   let mdStr = md1;
-//   mdStr += "Total Completed Courses: " + data['completed-courses'].length + ", Time: " + totalH + "h " + totalM + "m\n";
-//   mdStr += "<br/>\n";
-//   mdStr += "<br/>\n";
-//   mdStr += "<br/>\n";
-//   mdStr += "\n";
-//   data['completed-courses'].forEach(entry => {
-//     mdStr += "\n";
-//     if (entry['img']) {
-//       mdStr += "![thumbnail](" + entry['img'] + ")\n";
-//     }
-//     mdStr += "\n";
-//     mdStr += "[" + entry['title'] + "](" + entry['link'] + ")\n";
-//     mdStr += "- Author: " + entry['author'] + "\n";
-//     //mdStr += "- Released: " + entry['released-date'] + "\n";
-//     mdStr += "- Duration: " + entry['duration'] + "\n";
-//     mdStr += "- Completed: " + entry['completed-date'] + "\n";
-//     mdStr += "- Details: " + entry['details'] + "\n";
-//     mdStr += "- [top](#top) / [bottom](#bottom)\n";
-
-//     mdStr += "<br/>\n";
-//     mdStr += "<br/>\n";
-//     mdStr += "<br/>\n";
-//       mdStr += "\n";
-//   });
-//   mdStr += md2;
-//   fs.writeFileSync(MD_FILE, mdStr);
-// }
+    mdStr += "<br/>\n";
+    mdStr += "<br/>\n";
+    mdStr += "<br/>\n";
+      mdStr += "\n";
+  });
+  mdStr += md2;
+  fs.writeFileSync(MD_FILE, mdStr);
+}
 
 const main = async () => {
   // INTERNAL OPTIONS
@@ -182,7 +175,7 @@ const main = async () => {
     forceFullGather:  true,     // skip test for number of course
     scrollToBottom:   true,     // scroll page to bottom (WARNING: non-visible thumbnails are not loaded until page is scrolled)
     gatherDetails:    true,     // parse the details
-    useSampleData:   false,     // skip browser and use sample data file
+    useSampleData:   true,     // skip browser and use sample data file
     saveSampleData:   true,     // save to sample data file
     screenshot:      false,     // take snapshots
     screenshotDir:   SCREENSHOT_DIR
@@ -200,15 +193,15 @@ const main = async () => {
   await base.browser_close(browser);
 
   //DEBUG
-  console.log("data:");
-  console.log(JSON.stringify(data, null, space=2));
+  // console.log("data:");
+  // console.log(JSON.stringify(data, null, space=2));
 
-  // if (data['completed-courses'].length > 0) {
-  //   [totalH, totalM] = build_hours_minutes(data);
-  //   data['completed-courses'].sort((a, b) => (a['completed-ts'] < b['completed-ts']) ? 1 : -1) // decending
-  //   build_html(data, totalH, totalM);
-  //   build_md(data, totalH, totalM);
-  // }
+  if (data['completed-courses'].length > 0) {
+    [totalH, totalM] = build_hours_minutes(data);
+    data['completed-courses'].sort((a, b) => (a['completed-ts'] < b['completed-ts']) ? 1 : -1) // decending
+    build_html(data, totalH, totalM);
+    build_md(data, totalH, totalM);
+  }
 
   console.log("done.");
 };
